0c9b2d631ab6c4b2f93d3e37f664ec130f2702c9
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-01-11 17:08:26 +0000

    refactor and test: lockbox2 implementation and tests

--- lockbox2/programs/liquidity_lockbox/src/state.rs
+++ lockbox2/programs/liquidity_lockbox/src/state.rs
@@ -10,16 +10,14 @@ pub struct LiquidityLockbox {
   pub fee_collector_token_owner_account_a: Pubkey,
   // Fee collector ATA for token B
   pub fee_collector_token_owner_account_b: Pubkey,
-  // Total liquidity in a lockbox
-  // Considering OLAS and SOL inflation, it will never practically be bigger than 2^64 - 1
-  pub total_liquidity: u64,
-  // Total number of lockbox positions
-  // Even if position is created every second, it would take 136+ years to create 2^32 - 1 positions
-  pub num_positions: u32
+  // Liquidity position
+  pub position: Pubkey,
+  // PDA position ATA
+  pub pda_position_account: Pubkey
 }
 
 impl LiquidityLockbox {
-  pub const LEN: usize = 8 + 1 + 32 * 3 + 8 + 4;
+  pub const LEN: usize = 8 + 1 + 32 * 5;
 
   pub fn seeds(&self) -> [&[u8]; 2] {
     [
@@ -33,57 +31,16 @@ impl LiquidityLockbox {
     bump: u8,
     bridged_token_mint: Pubkey,
     fee_collector_token_owner_account_a: Pubkey,
-    fee_collector_token_owner_account_b: Pubkey
+    fee_collector_token_owner_account_b: Pubkey,
+    position: Pubkey,
+    pda_position_account: Pubkey
   ) -> Result<()> {
     self.bridged_token_mint = bridged_token_mint;
     self.fee_collector_token_owner_account_a = fee_collector_token_owner_account_a;
     self.fee_collector_token_owner_account_b = fee_collector_token_owner_account_b;
-    self.total_liquidity = 0;
-    self.num_positions = 0;
+    self.position = position;
+    self.pda_position_account = pda_position_account;
     self.bump = [bump];
-
-    Ok(())
-  }
-}
-
-#[account]
-pub struct LockboxPosition {
-  // Position identifier
-  pub id: [u8; 4],
-  // Position bump
-  pub bump: [u8; 1],
-  // Locked position data account
-  pub position_account: Pubkey,
-  // Locked position PDA ATA
-  pub position_pda_ata: Pubkey,
-  // Locked position liquidity
-  pub position_liquidity: u64
-}
-
-impl LockboxPosition {
-  pub const LEN: usize = 8 + 4 + 1 + 32 + 32 + 8;
-
-  pub fn seeds(&self) -> [&[u8]; 3] {
-    [
-      &b"lockbox_position"[..],
-      self.id.as_ref(),
-      self.bump.as_ref()
-    ]
-  }
-
-  pub fn initialize(
-    &mut self,
-    id: u32,
-    bump: u8,
-    position_liquidity: u64,
-    position_account: Pubkey,
-    position_pda_ata: Pubkey
-  ) -> Result<()> {
-    self.id = id.to_be_bytes();
-    self.bump = [bump];
-    self.position_liquidity = position_liquidity;
-    self.position_account = position_account;
-    self.position_pda_ata = position_pda_ata;
 
     Ok(())
   }
